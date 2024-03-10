3a34ae6889fb86bef09beea493f278a74c9a22f9
joshua-a-a <154514805+joshua-a-a@users.noreply.github.com>
2024-03-10 20:58:07 +0000

    fixed issues for Holiday.py
    addressed issues were city_flight and holiday_cost functions, I have updated and marked on code where and what changes were made

--- Holiday.py
+++ Holiday.py
@@ -1,70 +1,31 @@
 
-# take user input for : 
-    # city_flight , num_nights , rental_days
 
-city_flight = {"Bangkok"  : 1000, 
-               "Paris"    : 400,
-               "Madrid"   : 600,
-               "Kingston" : 800
-               }
+city_list = {"Bangkok": 1000, "Paris": 400, "Madrid": 600, "Kingston": 800}
 
-city_flight = input("Where will you be flying to? \n1. Bangkok, \n2. Paris, \n3. Madrid, \n4. Kingston. \nEnter here: ")
-city_flight = city_flight.capitalize()
-num_nights = int(input("\nHow many nights will you need a hotel? "))
-rental_days = int(input("\nHow many days will you rent a car for?: "))
-
-# create the following four functions : 
-
-# 1: hotel_cost with the argument (num_nights)
-
-def hotel_cost(num_nights):
-    return num_nights * 100 # this is the cost of the hotel per night 
-
-# 2: plane_cost with the argument (city_flight)
-
-def plane_cost(city_flight):
-    ticket = 0 # the value of the ticket will vary depending on the chosen city travelling to 
-
+def city_flight():
     while True:
-            
-        if city_flight == "Bangkok": # lets the user enter the city name or the number associated 
-            ticket = 1000
-            return ticket
-        elif city_flight == "Paris":
-            ticket = 400
-            return ticket
-
-        elif city_flight == "Madrid":
-            ticket = 600
-            return ticket
-
-        elif city_flight == "Kingston":
-            ticket = 800
-            return ticket
-        
-        elif TypeError: 
-            print("That isn't the name of a city! Try again :)")
-            return False
-        
+        city = input("Please enter your destination city: ")
+        city = city.capitalize()
+        if city in city_list:
+            return city_list[city]
         else:
-            print("You have selected an invalid option. Try again :)")
-            return False            
+            print("Invalid city. Please choose from the following cities: ", ', '.join(city_list.keys()))
 
-# 3: car_rental with the argument (rental_days)
+def hotel_cost():
+    num_nights = int(input("How many nights will you be staying? "))
+    return num_nights * 70
 
-def car_rental(rental_days):
+def car_rental():
+    rental_days = int(input("How many days will you be renting a car? "))
     return rental_days * 50
 
-# Print out all the details about the holiday in a readable way
+def holiday_cost():
+    plane_cost = city_flight()
+    hotel_cost_value = hotel_cost()
+    car_rental_cost = car_rental()
+    total = plane_cost + hotel_cost_value + car_rental_cost
+    print(f"\nYour flight will cost: £{plane_cost}. \nYour hotel cost is: £{hotel_cost_value}. \nAnd your car rental will cost: £{car_rental_cost}.\n")
+    print(f"The total of the holiday comes to: £{total}")
 
-def holiday_cost(num_nights, city_flight, rental_days):
-    num_nights = hotel_cost(num_nights)
-    city_flight = plane_cost(city_flight)
-    rental_days = car_rental(rental_days)
-    
-    print(f"\nYour flight will cost: £{city_flight}. \nYour hotel cost is: £{num_nights}. \nAnd your car rental will cost: £{rental_days}.\n")
-    return num_nights + city_flight + rental_days
-
-total = holiday_cost(num_nights, city_flight, rental_days)
-
-print(f"The total of the holiday comes to: £{total}")
+# Call the function
+holiday_cost()
